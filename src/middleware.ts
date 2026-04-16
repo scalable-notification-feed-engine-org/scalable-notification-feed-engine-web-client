a6089c8a7e558347024ca98dcf38290087f56d09
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-16 22:30:57 +0530

    feat(security): implement centralized auth-guard middleware for session orchestration

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import {NextRequest, NextResponse} from "next/server";
+
+export function middleware(request:NextRequest){
+    const token = request.cookies.get('auth_token')?.value;
+
+    console.log(request.nextUrl);
+
+    const { pathname } = request.nextUrl;
+
+    if(!token && pathname.startsWith('/dashboard')){
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+
+    if(token && (pathname === '/login' || pathname === '/register')){
+        return NextResponse.redirect(new URL('/dashboard', request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: ['/dashboard/:path*','/login', '/register'],
+};
