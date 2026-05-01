0ec8f011e2add2444133aaaa4f389d7257d19527
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-05-01 22:32:37 +0530

    feat(ui): add navigation icons for mailbox, groups, and users to Navbar

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { NotificationBell } from './NotificationBell';
 import { useAuth } from '@/context/AuthContext';
+import {Group, Mailbox, Users} from "lucide-react";
+import Link from "next/link";
 
 export const Navbar = () => {
     const { user, logout } = useAuth();
@@ -11,6 +13,16 @@ export const Navbar = () => {
         <nav className="flex justify-between items-center px-8 py-4 bg-white shadow-sm border-b">
             <div className="text-xl font-bold text-blue-600">
                 ActivityHub
+            </div>
+
+            <div>
+                <div className="flex gap-4">
+                    <Mailbox />
+                    <Link href="/group" className="hover:text-blue-600 transition duration-200">
+                    <Group/>
+                    </Link>
+                    <Users/>
+                </div>
             </div>
 
             <div className="flex items-center gap-6">
