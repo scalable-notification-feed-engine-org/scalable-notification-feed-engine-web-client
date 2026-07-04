d77ae4bbb1ecb5c7b78f8aea95dbbb5d3cb62fb2
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-07-04 13:40:38 +0530

    chore(apollo): remove debug logs and update GraphQL endpoint configuration

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -3,7 +3,7 @@ import Cookies from "js-cookie";
 import { ApolloLink, Observable } from "@apollo/client";
 
 const httpLink = new HttpLink({
-    uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
+    uri: "http://localhost:3004/graphql",
 });
 
 const errorLink = new ApolloLink((operation, forward) => {
@@ -11,7 +11,6 @@ const errorLink = new ApolloLink((operation, forward) => {
            const subscription = forward(operation).subscribe({
 
             next: (response) => {
-                console.log("Response GRAPHQL" ,response);
                 if (response.errors) {
                     response.errors.forEach(({ message, extensions }) => {
                         console.error(`[GraphQL error]: Message: ${message}`);
@@ -22,7 +21,6 @@ const errorLink = new ApolloLink((operation, forward) => {
                         }
                     });
                 }
-                console.log("Graphql response " , response);
                 observer.next(response);
             },
             error: (networkError) => {
