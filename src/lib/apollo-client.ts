31d3ccd293e044c45240b434843c73c0d3dbfadc
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-07-23 11:14:05 +0530

    feat: enhance Apollo client with dynamic authLink and include x-user-id header; handle UNAUTHENTICATED by clearing auth_token and user_id cookies

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,6 +1,5 @@
-import {ApolloClient,HttpLink, InMemoryCache} from "@apollo/client";
+import { ApolloClient, HttpLink, InMemoryCache, ApolloLink, Observable } from "@apollo/client";
 import Cookies from "js-cookie";
-import { ApolloLink, Observable } from "@apollo/client";
 
 const httpLink = new HttpLink({
     uri: "http://localhost:3004/graphql",
@@ -8,15 +7,16 @@ const httpLink = new HttpLink({
 
 const errorLink = new ApolloLink((operation, forward) => {
     return new Observable((observer) => {
-           const subscription = forward(operation).subscribe({
-
+        const subscription = forward(operation).subscribe({
             next: (response) => {
+                console.log("RESPONSE", response);
                 if (response.errors) {
                     response.errors.forEach(({ message, extensions }) => {
                         console.error(`[GraphQL error]: Message: ${message}`);
 
                         if (extensions?.code === 'UNAUTHENTICATED') {
                             Cookies.remove('auth_token');
+                            Cookies.remove('user_id');
                             window.location.href = '/login';
                         }
                     });
@@ -34,22 +34,24 @@ const errorLink = new ApolloLink((operation, forward) => {
     });
 });
 
-const authLink = new ApolloLink((operation, forward) =>{
+export const createApolloClient = (dbUserId: string) => {
+    const authLink = new ApolloLink((operation, forward) => {
+        const token = Cookies.get('auth_token');
+        console.log("USER ID APPLO", dbUserId);
 
-    const token = Cookies.get('auth_token');
+        operation.setContext(({ headers = {} }) => ({
+            headers: {
+                ...headers,
+                authorization: token ? `Bearer ${token}` : '',
+                'x-user-id': dbUserId || '',
+            }
+        }));
 
+        return forward(operation);
+    });
 
-    operation.setContext(({headers = {} }) => ({
-        headers: {
-            ...headers,
-            authorization: `Bearer ${token}`,
-        }
-    }));
-    return forward(operation);
-});
-
-
-export const client = new ApolloClient({
-    link: ApolloLink.from([errorLink, authLink, httpLink]),
-    cache: new InMemoryCache(),
-})
+    return new ApolloClient({
+        link: ApolloLink.from([errorLink, authLink, httpLink]),
+        cache: new InMemoryCache(),
+    });
+};
